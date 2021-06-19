60ce752541c0ba7463406ed8eb57e0bfc52649e9
Your Name <arthurresendefaria@MacBook-Pro.local>
2021-06-19 00:03:30 -0300

    os números podes trocar de lugar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Tiles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tiles: [[0, 1, 2], [3, 4, 5]],
+    }
+  }
+
+  changeTiles(row, column) {
+    let tilesCopy = this.state.tiles
+    let temp = tilesCopy[row][column];
+
+    tilesCopy[row][column] = tilesCopy[row][column + 1];
+    tilesCopy[row][column + 1] = temp;
+    temp = null;
+
+    this.setState({
+      tiles: tilesCopy,
+    });
+  }
+
+  render() {
+    return(
+      <div className="tiles">
+        <h1>{this.state.tiles[0]}</h1>
+        <h1>{this.state.tiles[1]}</h1>
+        <button onClick={() => this.changeTiles(0, 0)}>eh</button>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Tiles></Tiles>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
