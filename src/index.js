e1785da4fb7a762f0a99f22e94d96199c605832d
Your Name <arthurresendefaria@MacBook-Pro.local>
2021-06-23 16:15:41 -0300

    dependência eslint adicionada, levando em conta a necessidade de ter um código limpo.

--- src/index.js
+++ src/index.js
@@ -1,135 +1,118 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
 import '../src/index.css'
 
 class Tiles extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       size: this.props.size,
       tiles: Array.from(
-        {length: (this.props.size**2)}, (v, k) => {
-          if(k!==this.props.size**2-1){return k + 1} else {return 0}
+        { length: (this.props.size ** 2) }, (v, k) => {
+          if (k !== this.props.size ** 2 - 1) { return k + 1 } else { return 0 }
         }),
-      indexZero: this.props.size**2-1,
-      winner: false,
+      indexZero: this.props.size ** 2 - 1,
+      winner: false
     }
   }
 
-  didItWin() {
-    const tiles = this.state.tiles;
+  didItWin () {
+    const tiles = this.state.tiles
     const winningOrder = Array.from(
-      {length: (this.props.size**2)}, (v, k) => {
-        if(k!==this.props.size**2-1){return k + 1} else {return 0}
-      });
+      { length: (this.props.size ** 2) }, (v, k) => {
+        if (k !== this.props.size ** 2 - 1) { return k + 1 } else { return 0 }
+      })
 
-    for(let i=0; i<tiles.length; i++) {
-      if(tiles[i] !== winningOrder[i]) {
-        return false;
+    for (let i = 0; i < tiles.length; i++) {
+      if (tiles[i] !== winningOrder[i]) {
+        return false
       }
     }
 
-    alert('you won');
+    alert('you won')
   }
 
-  move(position) {
-    let tiles = this.state.tiles;
-    const indexZero = this.state.indexZero;
-
-    switch(position) {
-      case 'r':
-        tiles[indexZero] = tiles[indexZero + 1];
-        tiles[indexZero + 1] = 0;
-        this.setState({
-          tiles: tiles,
-          indexZero: indexZero + 1,
-        });
-        break;
-      
-      case 'l':
-        tiles[indexZero] = tiles[indexZero - 1];
-        tiles[indexZero - 1] = 0;
-        this.setState({
-          tiles: tiles,
-          indexZero: indexZero - 1,
-        });
-        break;
-
-      case 'u':
-        tiles[indexZero] = tiles[indexZero - this.state.size];
-        tiles[indexZero - this.state.size] = 0;
-        this.setState({
-          tiles: tiles,
-          indexZero: indexZero - this.state.size,
-        });
-        break;
-
-      case 'd':
-        tiles[indexZero] = tiles[indexZero + this.state.size];
-        tiles[indexZero + this.state.size] = 0;
-        this.setState({
-          tiles: tiles,
-          indexZero: indexZero + this.state.size,
-        });
-        break;
-      
-      default:
-        break;
-    }
-  }
-
-  findDirection(tile) {
+  move (position) {
     const tiles = this.state.tiles
     const indexZero = this.state.indexZero
 
-    if(tile === tiles[indexZero - 1])
-      this.move('l')
-    else if(tile === tiles[indexZero + 1])
-      this.move('r')
-    else if(tile===tiles[indexZero - this.state.size])
-      this.move('u')
-    else if(tile===tiles[indexZero + this.state.size])
-      this.move('d')
-    else
-      return
+    tiles[indexZero] = tiles[position]
+    tiles[position] = 0
+    this.setState({
+      tiles: tiles,
+      indexZero: position
+    })
   }
 
-  render() {
-    return(
+  findDirection (tile) {
+    const tiles = this.state.tiles
+    const indexZero = this.state.indexZero
+
+    switch (tile) {
+      case tiles[indexZero - 1]:
+        this.move(indexZero - 1)
+        break
+
+      case tiles[indexZero + 1]:
+        this.move(indexZero + 1)
+        break
+
+      case tiles[indexZero - this.state.size]:
+        this.move(indexZero - this.state.size)
+        break
+
+      case tiles[indexZero + this.state.size]:
+        this.move(indexZero + this.state.size)
+        break
+
+      default:
+        break
+    }
+  }
+
+  render () {
+    return (
       <div>
         {this.state.tiles.map((tile) => (
           <button
-            className="tile"
-            id={'number' + tile}
-            onClick={() => {this.findDirection(tile); this.didItWin()}}
+            key={tile}
+            className = "tile"
+            id = { 'number' + tile }
+            onClick={ () => { this.findDirection(tile); this.didItWin() } }
           >
             {tile}
           </button>
         ))}
       </div>
-    );
+    )
   }
 }
 
+Tiles.propTypes = {
+  size: PropTypes.number.isRequired
+}
+
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      sizeOfGame: 4,
+      sizeOfGame: 4
     }
   }
 
-  render() {
+  render () {
     return (
       <div className="board">
         <Tiles size={this.state.sizeOfGame}></Tiles>
         {this.state.sizeOfGame}
       </div>
-    );
+    )
   }
 }
 
 ReactDOM.render(
   <Board></Board>,
   document.getElementById('root')
-);
+)
